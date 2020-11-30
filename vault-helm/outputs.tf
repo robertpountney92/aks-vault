bb71bda5395ccb9b2bbccbeceec471fb08123cc6
robertpountney92 <robertpountney92@gmail.com>
2020-11-30 10:16:27 +0000

    Modified output names and removed unwanted README

--- vault-helm/outputs.tf
+++ vault-helm/outputs.tf
@@ -1,10 +1,10 @@
-output "client_id" {
+output "sp_client_id" {
   value = azuread_service_principal.vault.application_id
 }
 output "tenant_id" {
   value = data.azurerm_client_config.current.tenant_id
 }
-output "azurerm_client_config" {
+output "az_user_client_id" {
   value = data.azurerm_client_config.current.client_id
 }
 
